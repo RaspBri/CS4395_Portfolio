4f7add914c83d04dfced50b708dff30d2d8e88e8
Brianna Murray <42558498+RaspBri@users.noreply.github.com>
2023-03-07 21:23:57 -0600

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -11,6 +11,7 @@ from nltk.corpus import stopwords
             Website: Texas Tulips
 """
 
+# Go through given url and save text to .txt file
 def scrapeText(URL, count):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,60 +19,56 @@ def scrapeText(URL, count):
     for p in soup.select('p'):
         file = open(('link' + str(count) + '.txt'), "a")
         file.write(p.get_text())
-        #print(p.get_text())
 
 
+# remove newlines and tabs from .txt files
 def cleanText():
-    # remove newlines
-    text_no_line_breaks = ''
+    textNoLineBreaks = ''
     for i in range(1, 15):
         file = open('link' + str(i) + '.txt')
         for eachLine in file:
             stripped_line = eachLine.rstrip('\t\n')
-            text_no_line_breaks += stripped_line
+            textNoLineBreaks += stripped_line
 
 
+# tokenize sentences from each .txt file and save to new file ending w/ tokens.txt
 def tokenizeSentences():
     for i in range(1, 15):
         file = open('link' + str(i) + '.txt').read()
-        # tokenize sentences and lowercase
-        tokens = sent_tokenize(file.lower())
-        # write tokens to new file
-        outFile = open(('link' + str(i) + 'tokens.txt'), "w")
-        #print(tokens)
+        tokens = sent_tokenize(file.lower()) # tokenize sentences and lowercase
+        outFile = open(('link' + str(i) + 'tokens.txt'), "w") # write tokens to new file
         outFile.writelines(tokens)
 
-
+# tokenize words by removing stopwords, punctuation, and lowercase everything
 def tokenizeWords():
     wordList = []
     for i in range(1, 15):
-        file = open('link' + str(i) + 'tokens.txt').read() # read sentences in as tokens
+        file = open('link' + str(i) + 'tokens.txt').read()
         tokens = word_tokenize(file) # break up sentences into tokens of words
-        # tokenize words by removing stop words, punctuation, and lowercase everything
         tokens = [t for t in tokens if t.isalpha() and t.lower() and
                   t not in stopwords.words('english')]
         for t in tokens:
-            wordList.append(t)
+            wordList.append(t) # add tokens from each sentence to list
 
-        """ Surprising how few words made it through to be saved into the .txt file """
+        """ Surprising how few words made it through """
 
         outFile = open(('link' + str(i) + 'tokens.txt'), "w")
         for item in tokens:
             outFile.write(item) # write string of tokens to .txt file
             outFile.write("\n")
 
-    wordList = list(set(wordList))
+    wordList = list(set(wordList)) # get only unique words from tokenized text
     return wordList
 
 
+# Term Frequency for each page from .txt file. Must convert strings to tokens again
 def findTermFreq():
     listDict = []
     for i in range(1, 15):
         tfDict = {}
-        # read all strings from file
         with open(('link' + str(i) + 'tokens.txt'), "r") as f:
             text = f.read()
-        tokens = word_tokenize(text) # tokenize text from each file
+        tokens = word_tokenize(text) # tokenize text from each file b/c it was saved as a string
 
         # go through all tokens and calculate term frequency for each word
         for word in tokens:
@@ -85,12 +82,12 @@ def findTermFreq():
             tfDict[word] = tfDict[word] / len(tokens)
 
         listDict.append(tfDict) # add to list for each page
-        #print(list(tfDict.values())[2])
-        #print(tfDict.values()) # print entire range of dict values for terms
     return listDict
 
 
-def findInverseDocFreq(tfDict, vocList):
+# Find Inverse Document Frequency for each page (AKA how often the word appears across all pages)
+# IDF = log(total number of documents in corpus) / (number of documents in corpus that contain term)
+def findInverseDocFreq(tfDict, vocList, numDocs):
     urlVoc = []
     idfDict = {}
 
@@ -98,32 +95,31 @@ def findInverseDocFreq(tfDict, vocList):
     for item in tfDict:
         urlVoc.append(item.keys())
 
-    #for term in allVoc
     for term in vocList:
         temp = ['x' for voc in urlVoc if term in voc]
-        idfDict[term] = math.log((1 + 14) / (1 + len(temp)))
-        #print(idfDict[term])
-    #print(idfDict)
+        idfDict[term] = math.log((1 + numDocs) / (1 + len(temp))) # adding 1 to prevent diving by 0
     return idfDict
 
 
+# tf-idf shows importance of a term for a page in the corpus
 def create_tfidf(tf, idf):
-    # tf = dictionary from page
-    # idf = single dictionary all unique words from every page
-    # td-idf closer to 0 = less important
+    """
+        tf = dictionary from page
+        idf = single dictionary all unique words from every page
+        td-idf closer to 0 = less important
+    """
+    
     tf_idf = {}
     for t in tf.keys():
        tf_idf[t] = tf[t] * idf[t]
     term_weight = sorted(tf.items(), key = lambda x:x[1], reverse = True) # highest tf-idf
-    #term_weight = sorted(tf.items(), key = lambda x:x[1]) # lowest tf-idf
-    # print(len(tf_idf))
+    # term_weight = sorted(tf.items(), key = lambda x:x[1]) # lowest tf-idf
     return tf_idf, term_weight
 
 
 if __name__ == '__main__':
-    # Topic = Plants
-    # Website, Texas Tulips
-    # Small local plant shops did not have enough URLs for the project, using this website instead
+
+    # Smaller local plant shops did not have enough URLs for the project, using this website instead
     URL = 'https://texas-tulips.com/?utm_source=local&utm_medium=organic&utm_campaign=gmb'
 
     page = requests.get(URL)
@@ -134,21 +130,21 @@ if __name__ == '__main__':
     urlList = []
     for link in soup.find_all('a'):
         counter += 1
-        if counter > 20:
+        if counter > 20: # get 20 urls
             break
-        #scrapeText(link.get('href'), counter)
         urlList.append(link.get('href'))
-    urlList = list(set(urlList)) # do not use same url
+    urlList = list(set(urlList)) # get only unique urls
+    numDocs = len(urlList) # will be used in findInverseDocFreq()
 
     for i in range(1, (len(urlList) + 1), 1): # range(start, stop, step)
-        scrapeText(link.get('href'), i)
+        scrapeText(link.get('href'), i) # scrape each url for text
 
 
     cleanText() # remove newlines
     tokenizeSentences() # make everything lowercase
     vocList = tokenizeWords() # remove stopwords & punctuation, return list of unique words
     tfDict = findTermFreq() # return term frequency as list with dictionaries for each page
-    idfDict = findInverseDocFreq(tfDict, vocList) # return inverse document frequency
+    idfDict = findInverseDocFreq(tfDict, vocList, numDocs) # return inverse document frequency
 
     # iterate through all term freq dicts for each page
     for i in range(len(tfDict)):
@@ -162,7 +158,7 @@ if __name__ == '__main__':
     picking, april, visit, times, pay, parking
     """
 
-    # mini knowledge base w/ basic facts to build off of later
+    # mini knowledge base w/ basic facts to build out later
     knowledgeBase = {
         'tulips': ['Tulips come in many colors such as: purple, red, orange, yellow, white, and pink',
                  'Tulip Care: Cut stems and place in fresh water', 'Types of tulips on the grounds: 100 varieties of tulips'],
