8f66565c04421dfc926f6522fbff1202583e9f33
Brianna Murray <42558498+RaspBri@users.noreply.github.com>
2023-03-07 20:39:53 -0600

    Update program1.py

--- N_Gram/program1.py
+++ N_Gram/program1.py
@@ -1,36 +1,181 @@
+import requests
+import math
+from bs4 import BeautifulSoup
 import pickle
-import nltk
-from nltk import word_tokenize
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+
+""" 
+            MUST HAVE FEW LINKS FROM OUTSIDE THE DOMAIN (GO FOR SOCIAL MEDIA PAGES)
+            - maybe global variable for the range?
+            - update all variableLikeThis to variable_like_this
+"""
+
+def scrapeText(URL, count):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for p in soup.select('p'):
+        file = open(('link' + str(count) + '.txt'), "a")
+        file.write(p.get_text())
+        #print(p.get_text())
 
 
-# Read, remove new lines, make unigram, make bigrams
-def processText(file):
-    clean_file = file.read().replace('\n', '') # Read & Remove new lines
-    unigrams = word_tokenize(clean_file) # tokenize text to a unigram
-    bigrams = [(unigrams[k], unigrams[k + 1]) for k in range(len(unigrams) - 1)] # make bigram
-    return unigrams, bigrams
+def cleanText():
+    # remove newlines
+    text_no_line_breaks = ''
+    for i in range(1, 15):
+        file = open('link' + str(i) + '.txt')
+        for eachLine in file:
+            stripped_line = eachLine.rstrip('\t\n')
+            text_no_line_breaks += stripped_line
 
-# preprocess text, create unigram & bigram dictionary
-def runProgram1(filename):
-    file = open(filename, "r")
-    unigrams, bigrams = processText(file)  # Read & Remove new lines
-    bigramDictionary = {t: bigrams.count(t) for t in set(bigrams)} # Bigram dictionary [‘token1 token2’] -> count
-    unigramDictionary = {t: unigrams.count(t) for t in set(unigrams)} # Unigram dictionary [‘token1’] -> count
 
-    return unigramDictionary, bigramDictionary
+def tokenizeSentences():
+    for i in range(1, 15):
+        file = open('link' + str(i) + '.txt').read()
+        # tokenize sentences and lowercase
+        tokens = sent_tokenize(file.lower())
+        # write tokens to new file
+        outFile = open(('link' + str(i) + 'tokens.txt'), "w")
+        #print(tokens)
+        outFile.writelines(tokens)
 
-# create unigram & bigram pickle file
-def pickleDictionary(unigramDict, bigramDict, filename):
-    pickle.dump(unigramDict, open((filename + '_unigramDict.pickle'), 'wb')) # pickle unigram
-    pickle.dump(bigramDict, open((filename + '_bigramDict.pickle'), 'wb')) # pickle bigram
+
+def tokenizeWords():
+    wordList = []
+    for i in range(1, 15):
+        file = open('link' + str(i) + 'tokens.txt').read() # read sentences in as tokens
+        tokens = word_tokenize(file) # break up sentences into tokens of words
+        # tokenize words by removing stop words, punctuation, and lowercase everything
+        tokens = [t for t in tokens if t.isalpha() and t.lower() and
+                  t not in stopwords.words('english')]
+        for t in tokens:
+            wordList.append(t)
+
+        """ Surprising how few words made it through to be saved into the .txt file """
+
+        outFile = open(('link' + str(i) + 'tokens.txt'), "w")
+        for item in tokens:
+            outFile.write(item) # write string of tokens to .txt file
+            outFile.write("\n")
+
+    wordList = list(set(wordList))
+    return wordList
+
+
+def findTermFreq():
+    listDict = []
+    for i in range(1, 15):
+        tfDict = {}
+        # read all strings from file
+        with open(('link' + str(i) + 'tokens.txt'), "r") as f:
+            text = f.read()
+        tokens = word_tokenize(text) # tokenize text from each file
+
+        # go through all tokens and calculate term frequency for each word
+        for word in tokens:
+            if word in tfDict:
+               tfDict[word] += 1
+            else:
+                tfDict[word] = 1
+
+        # normalize the term frequency by number of tokens
+        for word in tfDict.keys():
+            tfDict[word] = tfDict[word] / len(tokens)
+
+        listDict.append(tfDict) # add to list for each page
+        #print(list(tfDict.values())[2])
+        #print(tfDict.values()) # print entire range of dict values for terms
+    return listDict
+
+
+def findInverseDocFreq(tfDict, vocList):
+    urlVoc = []
+    idfDict = {}
+
+    # take list of words from each dict in tfDict and put into list
+    for item in tfDict:
+        urlVoc.append(item.keys())
+
+    #for term in allVoc
+    for term in vocList:
+        temp = ['x' for voc in urlVoc if term in voc]
+        idfDict[term] = math.log((1 + 14) / (1 + len(temp)))
+        #print(idfDict[term])
+    #print(idfDict)
+    return idfDict
+
+
+def create_tfidf(tf, idf):
+    # tf = dictionary from page
+    # idf = single dictionary all unique words from every page
+    tf_idf = {}
+    for t in tf.keys():
+       tf_idf[t] = tf[t] * idf[t]
+    #term_weight = sorted(tf.items(), key = lambda x:x[1], reverse = True) # highest tf-idf
+    term_weight = sorted(tf.items(), key = lambda x:x[1]) # lowest tf-idf // terms are more frequent
+    # print(len(tf_idf))
+    return tf_idf, term_weight
+
 
 if __name__ == '__main__':
+    # Topic = Plants
+    # Website, Texas Tulips
+    # Small local plant shops did not have enough URLs for the project, using this website instead
+    URL = 'https://texas-tulips.com/?utm_source=local&utm_medium=organic&utm_campaign=gmb'
 
-    unigramDictionary1, bigramDictionary1 = runProgram1("LangId.train.English")
-    pickleDictionary(unigramDictionary1, bigramDictionary1, "LangId.train.English")
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-    unigramDictionary2, bigramDictionary2 = runProgram1("LangId.train.French")
-    pickleDictionary(unigramDictionary2, bigramDictionary2, "LangId.train.French")
+    # Loop to find all URLs on Texas Tulips' website
+    counter = 0
+    urlList = []
+    for link in soup.find_all('a'):
+        counter += 1
+        if counter > 20:
+            break
+        #scrapeText(link.get('href'), counter)
+        urlList.append(link.get('href'))
+    urlList = list(set(urlList)) # do not use same url
 
-    unigramDictionary3, bigramDictionary3 = runProgram1("LangId.train.Italian")
-    pickleDictionary(unigramDictionary3, bigramDictionary3, "LangId.train.Italian")
+    for i in range(1, (len(urlList) + 1), 1): # range(start, stop, step)
+        scrapeText(link.get('href'), i)
+
+
+    cleanText() # remove newlines
+    tokenizeSentences() # make everything lowercase
+    vocList = tokenizeWords() # remove stopwords & punctuation, return list of unique words
+    tfDict = findTermFreq() # return term frequency as list with dictionaries for each page
+    idfDict = findInverseDocFreq(tfDict, vocList) # return inverse document frequency
+
+    # iterate through all term freq dicts for each page
+    for i in range(len(tfDict)):
+        tf_idf, termWeights = create_tfidf(tfDict[i], idfDict) # tf-idf value
+        #print(tf_idf) # print all terms from each page
+        print(termWeights) # print top 25 words from page
+
+    """
+    TOP 10 TERMS:
+    tulips, dallas, texas, season, contact,
+    february, april, visit, times, pay, parking
+    """
+
+    # mini knowledge base w/ basic facts to build off of later
+    knowledgeBase = {
+        'tulips': ['Tulips come in many colors such as: purple, red, orange, yellow, white, and pink',
+                 'Tulip Care: Cut stems and place in fresh water', 'Types of tulips on the grounds: 100 varieties of tulips'],
+        'dallas': ['Address: North of Dallas at 10656 FM2931, Pilot Point TX 76258'],
+        'texas': ['Address: North of Dallas at 10656 FM2931, Pilot Point TX 76258'],
+        'season': ['Open Season: During end of winter and early spring'],
+        'contact': ['Address: North of Dallas at 10656 FM2931, Pilot Point TX 76258',
+                  'Email: info@texas-tulips.com', 'Phone: 940-440-0232'],
+        'february': ['Open Season: During end of winter and early spring'],
+        'april': ['Open Season: During end of winter and early spring', 'End Season: Mid April'],
+        'visit': ['Hours: 10AM - 8PM Everyday', 'Address: North of Dallas at 10656 FM2931, Pilot Point TX 76258'] ,
+        'times': ['Hours: 10AM - 8PM Everyday'],
+        'pay': ['Entrance Fee: $5/person', 'Discounts for Veterans and Seniors: $7.50/person (includes three tulips & only during the week on business days)',
+                'Accepted forms of payment: Visa, MasterCard, and Cash'],
+        'parking': ['Parking Fee: FREE']
+    }
+    pickle.dump(knowledgeBase, open('knowledgeBase.pickle', 'wb')) # pickle database
