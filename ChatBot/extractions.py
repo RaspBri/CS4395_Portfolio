00ffe31eef87ae20ddeb3f91557870569af5d07c
Brianna Murray <42558498+RaspBri@users.noreply.github.com>
2023-04-15 05:38:54 -0500

    Update extractions.py

--- ChatBot/extractions.py
+++ ChatBot/extractions.py
@@ -1,191 +1,31 @@
-# Source Credit for tensorflow skeleton code, I added the rest
-# https://www.pycodemates.com/2021/11/build-a-AI-chatbot-using-python-and-deep-learning.html
-import string
-import nltk
-nltk.download('punkt')
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
-
-import numpy as np
-import tflearn
-from tensorflow.python.framework import ops
-import json
-import pickle
-import sys
-
+# Use ChatGPT to get website links relating to the topic the user wants to discuss
+# Use links for webscraping to build corpus for chatbot
 import openai
-import re, random
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-filename = sys.argv[1]
-print(filename)
-
-with open(filename) as file:
-    data = json.load(file)
-
-train = input("Train the model type in (train): ")
-# Data preprocessing start
-if train.lower() == "train":
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent['tag'])
-
-        if intent["tag"] not in labels:
-            labels.append(intent['tag'])
-
-    # Getting rid of duplicate words
-    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    '''
-    Turning words into a bag of words.
-    When feeding our model we want our words
-    to be numeric instead of string value.
-    So I'm going to do OHE the words turning
-    them to numeric values.
-
-    How we going to store it
-    word list: ['a', 'am', 'be']
-    OHE: [1, 0, 1]
-
-    Put a 1 if that word exist else put a 0
-    '''
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        # Bag of words
-        bag = []
-
-        wrds = [stemmer.stem(w) for w in doc]
-
-        # Going through all the words
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        # Look through label list look for tag and set that output to 1
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-    # turn them into arrays for easier model training
-    training = np.array(training)
-    output = np.array(output)
-
-    with open((filename +'.data.pickel'), 'wb') as f:
-        pickle.dump((words, labels, training, output), f)
-
-else:
-    with open((filename +'.data.pickel'), 'rb') as f:
-        words, labels, training, output = pickle.load(f)
-# Data preprocessing end
-
-# Model creation start
-ops.reset_default_graph()
-# Input layer trying to get the length from none on
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 16, activation="relu")  # hidden layer
-net = tflearn.fully_connected(net, 16, activation="relu")  # hidden layer
-# Output layer solfmax
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
-# end model creation
+import re
+import requests
+from bs4 import BeautifulSoup
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+import json
+import os
+import math
+import random
 
 
-# Start training
-'''
-training = input values
-output = output values
-n_epoch = how many times do I want to train same dataset
-batch_sizes = how many samples of data will pass through at a time 
-show_metric = Makes the output look nicer
-'''
-
-if train.lower() == "train":
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
-
-else:
-    model.load("model.tflearn")
 
 
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
 
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words if word not in "?"]
-
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-
-    return np.array(bag)
-
-def perform_lemmatization(tokens):
-    return [stemmer.stem(token) for token in tokens]
+"""
+To fix "unresolved reference" error for sklearn, run this command >>> pip install -U scikit-learn
+Run command in terminal before running >>> pip install nltk tensorflow tflearn
+Install >>> pip install jsbeautifier
+"""
 
 
-def get_processed_text(doc):
-    return perform_lemmatization(nltk.word_tokenize(doc.lower().translate((dict((ord(punctuation), None) for punctuation in string.punctuation)))))
-
-def generate_response(user_input, responses):
-    bot_response = ''
-    responses.append(user_input)
-
-    word_vectorizer = TfidfVectorizer(tokenizer=get_processed_text, stop_words='english')
-    all_word_vectors = word_vectorizer.fit_transform(responses)
-    similar_vector_vals = cosine_similarity(all_word_vectors[-1], all_word_vectors)
-    similar_sentence_num = similar_vector_vals.argsort()[0][2]
-
-    matched_vector = similar_vector_vals.flatten()
-    matched_vector.sort()
-    vector_matched = matched_vector[-2]
-
-    if vector_matched == 0:
-        print("cannot understand")
-    else:
-        print(responses[similar_sentence_num])
 
 
-def add_likes(likes_list):
-    data["intents"].append({
-        "tag": "likes",
-        "patterns": ["I like", "I love", "I enjoy", "favorite"],
-        "responses": likes_list
-    })
 
-def add_dislikes(dislikes_list):
-    data["intents"].append({
-        "tag": "dislikes",
-        "patterns": ["I don't like", "I do not like","I hate", "I don't enjoy", "I do not enjoy", "least favorite"],
-        "responses": dislikes_list
-    })
-
-def use_openai(mood):
+def use_openai(topic):
     openai.api_key = "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"
     model_engine = "gpt-3.5-turbo"
 
@@ -193,146 +33,218 @@ def use_openai(mood):
     response = openai.ChatCompletion.create(
         model = 'gpt-3.5-turbo',
         messages=[
-            {"role": "user", "content": ("what are some phrases someone who is {} would say. Play the role of a hard worker with these emotions".format(mood))}
+            {"role": "user", "content": ("Get 10 wikipedia links that tell me about " + topic)}
           ]
     )
     message = response.choices[0].message.content
-    message = re.findall(r'"([^"]*)"', message)
-    print(message)
-    return message
+    file = open("links.txt", "a")
+    file.write(message)
+    file.close()
 
 
-def choose_mood(number):
-    if number == 1:
-        mood = 'happy'
-    elif number == 2:
-        mood = 'mad'
-    elif number == 3:
-        mood = 'sad'
-    elif number == 4:
-        mood = 'mockery'
-    else:
-        mood = 'neutral'
-    return mood
 
-def sentiment(text):
-    sid_obj = SentimentIntensityAnalyzer()
-    sentiment_dict = sid_obj.polarity_scores(text)
 
-    print("Overall sentiment dictionary is : ", sentiment_dict)
-    print("sentence was rated as ", sentiment_dict['neg'] * 100, "% Negative")
-    print("sentence was rated as ", sentiment_dict['neu'] * 100, "% Neutral")
-    print("sentence was rated as ", sentiment_dict['pos'] * 100, "% Positive")
-    print("Sentence Overall Rated As", end=" ")
+#
+def write_to_file(soup):
+    for paragraph in soup.find_all('p'):
+        text = ''
+        text += paragraph.text
+        text = re.sub(r'\[\d+\]', '', text)  # remove mini reference numbers from lines
+        f = open((URL.rsplit('/', 1)[-1]) + '.txt', "a") # read page name from URL
+        f.write(text)
 
-    return sentiment_dict['neg'], sentiment_dict['neu'], sentiment_dict['pos']
+#
+def create_tf_dict(doc):
+    tf_dict = {}
+    tokens = word_tokenize(doc.lower().replace('\n', ' ').replace(',', ' '))
+    tokens = [t for t in tokens if t.isalpha() and t.lower() and
+              t not in stopwords.words('english')]
 
-def mock(user_input):
-    inp = list(user_input.lower())
-    for x in range(len(inp)):
-        if x % 2 == 0:
-            inp[x] = inp[x].lower()
+    sentence = sent_tokenize(doc)
+
+    # get term frequencies
+    for t in tokens:
+        if t in tf_dict:
+            tf_dict[t] += 1
         else:
-            inp[x] = inp[x].upper()
+            tf_dict[t] = 1
 
-    inp = ''.join(inp)
-    print(inp)
+    # get term frequencies in a more Pythonic way
+    token_set = set(tokens)
+    tf_dict = {t: tokens.count(t) for t in token_set}
 
-def user_checkin(user_fun_rating):
-    vader_neg, vader_neu, vader_pos = sentiment(user_fun_rating)
+    # normalize tf by number of tokens
+    for t in tf_dict.keys():
+        tf_dict[t] = tf_dict[t] / len(tokens)
 
-    # decide sentiment as positive, negative and neutral
-    if vader_pos >= 0.05:
-        print("Positive")
-        mood = 1
-    elif vader_neg >= 0.05:
-        print("Negative")
-        mood = random.randint(2, 4)
+    return tf_dict, sentence
+
+
+#
+def create_tfidf(tf, idf):
+    tf_idf = {}
+    for t in tf.keys():
+        tf_idf[t] = tf[t] * idf[t]
+
+    term_weight = sorted(tf.items(), key=lambda x: x[1], reverse=True)
+    return term_weight
+
+
+#
+def term_freq():
+    vocab = set()  # set of words
+    tf_dict_list = []
+    idf_dict = {}
+    all_words = []  # contains tf_dict.keys()
+    term_weight_list = []  # tf_idf for each file
+    sentences = []
+
+    # get tf_dict for each file
+    for item in file_names:
+        file_contents = open(item, 'r')
+        tf_dict, sentence = create_tf_dict(file_contents.read())
+        vocab = vocab.union(set(tf_dict.keys()))  # make set of all unique words
+        all_words += tf_dict.keys()  # get all words from every file
+        tf_dict_list.append(tf_dict)
+        sentences += sentence
+
+    for term in vocab:  # get idf
+        temp = ['x' for voc in all_words if term in voc]
+        idf_dict[term] = math.log((1 + len(file_names)) / (1 + len(temp)))
+
+    for item in range(len(file_names)):
+        term_weight = (create_tfidf(tf_dict_list[item], idf_dict))
+        term_weight_list.append(term_weight)
+
+    return vocab, tf_dict_list, idf_dict, all_words, term_weight_list, sentences
+
+
+#
+def add_intents(tag, temp_list, word_list):
+    data["intents"].append({
+        "tag": "{}".format(tag),  # get word to make the tag
+        "patterns": word_list,
+        "responses": temp_list.split('.')
+    })
+
+
+#
+def add_defaults(name):
+    data["intents"].append({
+        "tag": "thanks",
+        "patterns": ["Thanks", "Thank you"],
+        "responses": ["You're Welcome", "No problem"]
+    })
+    data["intents"].append({
+        "tag": "mood",
+        "patterns": ["How are you?", "What's up?"],
+        "responses": ["I am a bot."]
+    })
+    data["intents"].append({
+        "tag": "agree",
+        "patterns": ["Yes", "Yea", "Sure", "Ok"],
+        "responses": ["Awesome!", "Love to hear it!"]
+    })
+    data["intents"].append({
+        "tag": "disagree",
+        "patterns": ["No", "Never", "Eww", "Nah"],
+        "responses": ["Oh, my bad.", "Apologies."]
+    })
+    data["intents"].append({
+        "tag": "goodbye",
+        "patterns": ["Bye", "Goodbye", "See you later"],
+        "responses": ["Nice chatting, bye", "Bye, {}".format(name), "Goodbye, {}".format(name)]
+    })
+    data["intents"].append({
+        "tag": "greeting",
+        "patterns": ["Hi", "Hello", "Hey"],
+        "responses": ["Hi, {}!".format(name), "Hello", "Hey", "Good to see you, {}".format(name)]
+    })
+
+# skip over duplicate words adn omit the word "also"
+def check_list(word_list, seen):
+    final_list = []
+    for word in word_list:
+        count = seen.count(word[0])
+        if count < 1 and word[0] != 'also':
+            final_list.append(word[0])
+            seen.append(word[0])
+    return final_list, seen
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    data = {"intents": []}
+
+    print("Please enter your name...")
+    name = input("You: ")
+
+    # validate user
+    if os.path.isfile('{}_data.json'.format(name)):
+        print("Hi, {}".format(name))
+        cmd = "python extractions.py {}_data.json".format(name)
+
+        with open("{}_data.json".format(name)) as file:
+            data = json.load(file)
+
+        responses = []
+        for intent in data['intents']:
+            for pattern in intent['patterns']:
+                if 'I like' in intent['patterns']:
+                    responses = intent['responses']
+                elif 'I hate' in intent['patterns']:
+                    responses_d = intent['responses']
+        likes = random.choice(responses)
+        dislikes = random.choice(responses_d)
+        print("Welcome back! I remember last time you said you liked {}, I hope you got around to that after we chatted and avoided {}.".format(likes, dislikes))
+        os.system(cmd)
     else:
-        print("Neutral")
-        mood = 5
+        # get topic from user
+        print("What is your favorite hobby? ")
+        hobby = input("You: ")
 
-    mood = choose_mood(mood)
-    message_list = use_openai(mood)
+        use_openai(hobby) # get links and save to .txt file
+        print("Ok, I will learn about this...please wait")
+        # Read in links to files about user's interest
+        file = open("links.txt", "r")
 
-    if mood == 'mockery':
-        mock(user_fun_rating)
-    print(random.choice(message_list))
+        # Get URLs from .txt file and save them to their own file
+        file_names = []
 
 
+        for line in file:
+            URL = (re.search("(?P<url>https?://[^\s]+)", line).group("url"))
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            file_names.append((line.rsplit('/', 1)[-1]).strip() + ".txt")
+            write_to_file(soup)
+        vocab, tf_dict_list, idf_dict, all_words, term_weight_list, sentences = term_freq()
 
-# Generate user input
-def chat():
-    # user input
-    print("Start talking with bot!(type 'quit' to stop)")
-    likes_list = []
-    dislikes_list = []
-    chat_count = 0
+        """
+        to traverse through most frequent words
+        term_weight_list[x] where x is the document index you wish to see
+        """
 
-    while True:
+        # Make contents for .json file
+        temp_list = ''
+        seen_list = []
+        for count in range(len(file_names)): # get to file
+            for i in range(len(term_weight_list[count])): # go through all words from term weight
+                for sentence in sentences: # get to sentence level
+                    if term_weight_list[count][i][0] in sentence: # if word is found in sentence
+                        temp_list += sentence # add sentence to a list
+            word_list = (sorted(tf_dict_list[count].items(), key=lambda x: x[1], reverse=True))
+            word_list, seen_list_add = check_list(word_list, seen_list)
+            seen_list.append(seen_list_add)
+            add_intents(term_weight_list[count][i][0], temp_list, word_list[:5]) # add list of sentences to .json
+            temp_list = '' # clear list for next run
 
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            break
-        if chat_count == 5:
-            print("Are you having fun?")
-            user_fun_rating = input("You: ")
-            user_checkin(user_fun_rating)
+        # write dict to .json
+        add_defaults(name.lower())  # add default values to .json file
 
-            print("Are you ready to get back to learning? (y/n)")
-            learn_again = input("You: ")
-            if learn_again == 'y':
-                print("Let's get back to learning.")
-            elif learn_again == 'n':
-                print("What would you rather do?")
-                user_fun_rating = input("You: ")
-                user_checkin(user_fun_rating)
-                print("Let's get back to learning.")
-            chat_count = 0
-
-        elif 'dislike' in inp:
-            dislike = inp
-            dislike = dislike.rsplit('dislike ', 1)[1]
-            dislikes_list.append(dislike)
-            print("I see")
-        elif 'like' in inp:
-            like = inp
-            like = like.rsplit('like ', 1)[1]
-            likes_list.append(like)
-            print("I enjoy {} too".format(like))
-
-        else:
-            # All this is going to give us a matrix of numbers where the numbers are probabilities of each class
-            results = model.predict([bag_of_words(inp, words)])
-            # Argmax will grab the index of largest probability in the matrix
-            results_index = np.argmax(results)
-            tag = labels[results_index]
-
-            for tg in data["intents"]:
-                if tg['tag'] == tag:
-                    responses = tg['responses']
-
-            tfidf_vectorizer = TfidfVectorizer()
-            sparse_matrix = tfidf_vectorizer.fit_transform(responses)
-            doc_term_matrix = sparse_matrix.toarray()
-
-            tgt_transform = tfidf_vectorizer.transform([inp]).toarray()
-            tgt_cosine = cosine_similarity(doc_term_matrix, tgt_transform)
-            tgt_cosine_list = list(tgt_cosine)
-            i = tgt_cosine_list.index(max(tgt_cosine_list))
-            print(responses[i])
-        chat_count += 1
-
-    # add to .json file with likes and dislikes
-    if len(likes_list) > 0:
-        add_likes(likes_list)
-        with open(filename, 'w') as outfile: # Write the updated object back to the file
+        with open('{}_data.json'.format(name), 'w') as outfile:
             json.dump(data, outfile, indent=4)
 
-    if len(dislikes_list) > 0:
-        add_dislikes(dislikes_list)
-        with open(filename, 'w') as outfile: # Write the updated object back to the file
-            json.dump(data, outfile, indent=4)
-
-
-chat()
+        # get special filename
+        cmd = "python extractions.py {}_data.json".format(name)
+        os.system(cmd)
